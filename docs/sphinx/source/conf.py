6c048625b5ecfd8ccb3c128f41f531e78019a75e
Jon Webb <webbja123@gmail.com>
2023-09-11 21:17:19 -0600

    Updated revision number to 0.2.1

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2023, Jonathan A. Webb"
 author = "Jonathan A. Webb"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.2.1"
 
 
 # -- General configuration ---------------------------------------------------
